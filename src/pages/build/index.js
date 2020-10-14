d9749d28f9a897bc94a7f2fb26867277c3159576
Evandro Suzart <evandro.suzart@cmsw.com>
2020-10-14 14:27:20 -0300

    modularizaçao

---
+++ src/pages/build/index.js
@@ -0,0 +1,20 @@
+import  BonusBuild from './BonusBuild'
+import BuildHome from './BuildHome'
+import BuildProtections from './BuildProtections'
+import BuildStatus from './BuildStatus'
+import BuildWeapons from './BuildWeapons'
+import Cla from './Cla'
+import Guardioes from './Guardioes'
+import Habilidades from './Habilidades'
+import ItensAtalho from './ItensAtalho'
+
+
+export const  {BonusBuild,
+BuildHome,
+BuildProtections,
+BuildStatus,
+BuildWeapons,
+Cla,
+Guardioes,
+Habilidades,
+ItensAtalho,}
